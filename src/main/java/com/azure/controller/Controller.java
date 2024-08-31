29f7179f6d4f3168a3f4c8057c198c4068b70094
akashmapari9175 <itsakashmapari@gmail.com>
2024-08-31 23:16:21 +0530

    post method

--- src/main/java/com/azure/controller/Controller.java
+++ src/main/java/com/azure/controller/Controller.java
@@ -1,6 +1,8 @@
 package com.azure.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,9 @@ public class Controller {
 	@GetMapping("/get")
 	public String getMessage() {
 		return "done working correct";
+	}
+	@PostMapping("/send")
+	public String send(@RequestParam String name) {
+		return "hello"+name+"thank you ";
 	}
 }
